10dbeaea6d14228e36032bd6c5113d4087dc562b
Himanshu Kumar <36623672+H1manshu-Kumar@users.noreply.github.com>
2025-03-25 20:43:02 +0530

    Update delete_user.sh

--- delete_user.sh
+++ delete_user.sh
@@ -18,3 +18,28 @@ then
 else 
 	echo "the user was not deleted"
 fi
+
+#!/bin/bash
+
+<<helpText
+This shell script is created to delete the user
+helpText
+
+# Prompt the user to enter the username they want to delete and store it in the variable 'userName'
+read -p "Enter the username you want to delete " userName
+
+# Delete the user with the username stored in 'userName'
+sudo userdel "$userName"
+
+# Print a message indicating that the user has been successfully deleted
+echo "$userName User deleted successfully"
+
+# Check if the user still exists in the /etc/passwd file
+if [ $(cat /etc/passwd | grep $userName | wc | awk '{print $1}') == 0 ]
+then
+    # If the user does not exist (wc output is 0), print a confirmation message
+    echo "As wc is 0, the user is deleted"
+else 
+    # If the user still exists, print a message indicating the user was not deleted
+    echo "The user not deleted"
+fi
