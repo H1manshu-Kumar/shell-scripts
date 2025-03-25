d9c26d4d2adc36b9b34021e588741e73e6d840f7
Himanshu Kumar <36623672+H1manshu-Kumar@users.noreply.github.com>
2025-03-25 20:36:50 +0530

    :pencil: updated documentation for check_if_user_exists.sh

--- check_if_user_exists.sh
+++ check_if_user_exists.sh
@@ -4,13 +4,18 @@
 Check if username exists
 info
 
+# Prompt the user to enter the username they want to check
 read -p "Enter the username you want to check " username
 
+# Count the number of occurrences of the username in /etc/passwd
 count=$(cat /etc/passwd | grep $username | wc | awk '{print $1}')
 
+# Check if the count is 0, meaning the user does not exist
 if [ $count -eq 0 ]
 then 
-	echo "$username - user does not exist"
+    # Print message indicating the user does not exist
+    echo "$username - user does not exist"
 else
-	echo "$username - user exist!!"
+    # Print message indicating the user exists
+    echo "$username - user exist!!"
 fi
