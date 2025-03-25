ff925cccb30c150f2bc4783e68a99b5703d02681
Himanshu Kumar <36623672+H1manshu-Kumar@users.noreply.github.com>
2025-03-25 20:43:20 +0530

    :pencil: Update delete_user.sh

--- delete_user.sh
+++ delete_user.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-
-<<helpText
-This shell scrip is created to 
-delete the user
-helpText
-
-read -p "Enter the username you want to delete " userName
-
-sudo userdel "$userName"
-
-echo "$userName User deleted successfull"
-
-if [ $(cat /etc/passwd | grep $userName | wc | awk '{print $1}') == 0 ]
-then
-	echo "as wc is 0 the user is deleted"
-else 
-	echo "the user was not deleted"
-fi
-
-#!/bin/bash
-
 <<helpText
 This shell script is created to delete the user
 helpText
