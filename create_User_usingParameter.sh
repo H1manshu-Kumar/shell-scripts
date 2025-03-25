a13a4f32b5c02db070aecf90a81a21218f2fb43e
Himanshu Kumar <36623672+H1manshu-Kumar@users.noreply.github.com>
2025-03-25 20:41:03 +0530

    :pencil: documentation updated for create_User_usingParameter.sh

--- create_User_usingParameter.sh
+++ create_User_usingParameter.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-<<Agenda_Of_This_Script
+<<Script_Info
 This shell script is created to create a user using arguments
-Agenda_Of_This_Script
+Script_Info
 
+# Create a new user with the username provided as the first argument ($1)
 sudo useradd -m "$1"
 
+# Set the password for the new user using the second argument ($2)
 echo -e "$2\n$2" | sudo passwd "$1"
 
+# Print a message indicating that the user has been created with the provided arguments
 echo "====USER CREATED WITH ARGUMENT===="
