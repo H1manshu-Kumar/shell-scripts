449e2b6bdc666350a23c6a8114348432a13347f9
Himanshu Kumar <himanshu.kumar244@gmail.com>
2025-07-04 22:58:03 +0530

    Update Variables.sh :Pencil:

--- Variables.sh
+++ Variables.sh
@@ -1,32 +1,31 @@
-#!/bin/bash
+#!/bin/bash  # This line tells the system to use the bash shell to run this script
 
-# user-define variables
+# ---------------------------
+# 🧑‍💻 User-defined Variables
+# ---------------------------
+x="Ronaldo"  # Assigns the string "Ronaldo" to variable x
+y="Messi"    # Assigns the string "Messi" to variable y
 
-x="Ronaldo"
-y="Messi"
+echo "Player 1 =  $x"  # Prints the value of variable x
+echo "Player 2 = $y"   # Prints the value of variable y
 
-echo "Player 1 =  $x"
+# ---------------------------
+# 🌍 Shell/Environment Variables
+# ---------------------------
+echo "current logged-in user = $USER"  # Prints the username of the currently logged-in user
 
-echo "Player 2 = $y"
+# ---------------------------
+# 🎮 User Input
+# ---------------------------
+read -p "Enter your favourite player: " playerName  # Prompts the user to enter a value and stores it in playerName
+echo "Player 3 = $playerName"  # Prints the player entered by the user
 
-# shell/environment variables
+# ---------------------------
+# 🧾 Script Arguments
+# ---------------------------
+echo "Enter Game: $0"  # $0 refers to the script name itself
+echo "Enter Game Level: $1"  # $1 is the first argument passed to the script
+echo "Enter Mode (Easy/Medium/Hard): $2"  # $2 is the second argument
 
-echo "current logged-in user = $USER"
-
-#user input
-read -p "Enter you favourite player " playerName
-
-echo "Player 3 = $playerName"
-
-# arguments
-
-echo "Enter Game: $0"
-
-echo "Enter Game Level: $1"
-
-echo "Enter Mode (Easy/Medium/Hard): $2"
-
-echo "You have entered Game: $@"
-
-echo "toal number of arguments $#"
-
+echo "You have entered Game: $@"  # $@ prints all arguments as a single string
+echo "Total number of arguments: $#"  # $# prints the number of arguments passed to the script
