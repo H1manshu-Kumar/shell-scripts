1ce4fd9b546347818496b413c7c2da5478995d90
Himanshu Kumar <himanshu.kumar244@gmail.com>
2025-07-05 23:01:11 +0530

    Update loops_shell.sh

--- loops_shell.sh
+++ loops_shell.sh
@@ -10,8 +10,9 @@ Example: Printing numbers from 1 to 10
 - Increment = +1
 information
 
-# A for loop that runs from 1 to 10
+# A 'for' loop using C-style syntax
+# Initializes variable num=1; continues as long as num<=10; increments num by 1 each time
 for (( num=1 ; num<=10 ; num++ ))
 do
-    echo "$num"  # Prints the current value of num
+    echo "$num"  # Prints the current value of num to the terminal
 done
