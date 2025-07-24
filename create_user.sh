405b0b86c06fd3a1cc8054839949b7666716a7ff
Himanshu Kumar <himanshu.kumar244@gmail.com>
2025-07-24 18:41:32 +0530

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,25 +1,28 @@
-#!/bin/bash
+#!/bin/bash  # Specifies that this script should be run using the Bash shell
 
 <<helpText
 this is a shell script to create user
-
 helpText
+# Above is a multiline comment block (here-doc) used for internal script documentation
 
-#User Create
-
+# User Create
 function create_user {
-echo "======Creation of User======"
+  echo "======Creation of User======"
 
-read -p "enter the username: " userName
+  # Prompt the user to enter a username and store it in the variable 'userName'
+  read -p "enter the username: " userName
 
-read -p "enter the password: " password
+  # Prompt the user to enter a password and store it in the variable 'password'
+  read -p "enter the password: " password
 
-sudo useradd -m "$userName"
+  # Create a new user with a home directory using the provided username
+  sudo useradd -m "$userName"
 
-echo -e "$password\n$password" | sudo passwd "$userName"
+  # Set the password for the new user (echoing twice for confirmation)
+  echo -e "$password\n$password" | sudo passwd "$userName"
 
-echo "========User creation Completed======="
+  echo "========User creation Completed======="
 }
-#Call create_user function
-create_user
 
+# Call the create_user function to execute user creation steps
+create_user
