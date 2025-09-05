ccf86f477b8a9db7d172baf11334714ea820c991
H1manshu-Kumar <himanshu.kumar244@gmail.com>
2025-09-05 18:45:09 +0530

    added -s flag while reading password to hide it from input screen

--- create_user.sh
+++ create_user.sh
@@ -14,8 +14,8 @@ function create_user {
   # Prompt the user to enter a username and store it in the variable 'userName'
   read -p "enter the username: " userName
 
-  # Prompt the user to enter a password and store it in the variable 'password'
-  read -p "enter the password: " password
+  # Prompt the user to enter a password and store it in the variable 'password', -s flag hide the password from input screen
+  read -sp "enter the password: " password
 
   # Create a new user with a home directory using the provided username
   sudo useradd -m "$userName"
