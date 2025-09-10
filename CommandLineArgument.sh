e471e444f50aa74e6ca084e197d60f90efc6f311
H1manshu-Kumar <himanshu.kumar244@gmail.com>
2025-09-10 18:57:26 +0530

    added helptext to explain agenda of script :pencil:

--- CommandLineArgument.sh
+++ CommandLineArgument.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+<<helptext
+This scipt is created to demo command line argument during running the script
+helptext
+
 echo "Script Name: $0"
 echo "First Argument: $1"
 echo "Second Argument: $2"
