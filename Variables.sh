0c50f4f2eccb14e28236d7857069872403b72e6d
Himanshu Kumar <himanshu.kumar244@gmail.com>
2026-05-10 23:38:04 +0530

    Update Variables.sh

--- Variables.sh
+++ Variables.sh
@@ -1,28 +1,28 @@
 #!/bin/bash  # This line tells the system to use the bash shell to run this script
 
-# ---------------------------
+# --------------------------
 # 🧑‍💻 User-defined Variables
-# ---------------------------
+# --------------------------
 x="Ronaldo"  # Assigns the string "Ronaldo" to variable x
 y="Messi"    # Assigns the string "Messi" to variable y
 
 echo "Player 1 =  $x"  # Prints the value of variable x
 echo "Player 2 = $y"   # Prints the value of variable y
 
-# ---------------------------
+# --------------------------
 # 🌍 Shell/Environment Variables
-# ---------------------------
+# --------------------------
 echo "current logged-in user = $USER"  # Prints the username of the currently logged-in user
 
-# ---------------------------
+# --------------------------
 # 🎮 User Input
-# ---------------------------
+# --------------------------
 read -p "Enter your favourite player: " playerName  # Prompts the user to enter a value and stores it in playerName
 echo "Player 3 = $playerName"  # Prints the player entered by the user
 
-# ---------------------------
+# --------------------------
 # 🧾 Script Arguments
-# ---------------------------
+# --------------------------
 echo "Enter Game: $0"  # $0 refers to the script name itself
 echo "Enter Game Level: $1"  # $1 is the first argument passed to the script
 echo "Enter Mode (Easy/Medium/Hard): $2"  # $2 is the second argument
